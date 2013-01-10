f60192400d517833f57169f8a472b18a2307b1c6
Sávio S. Dias <wolfdragon2@gmail.com>
2013-01-09 23:52:38 -0200

    Criado Projeto e classe neurônio
    Foi criado e iniciado o projeto de implementação da rede neural, foi
    criado a classe "Neuron", que representará o neurônio artificial e
    conterá os algoritmos de aprendizado que trabalham em cima dele.

---
+++ Neural Network/Neuron.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural_Network
+{
+    class Neuron
+    {
+        // Attributes
+        private static int bias = 1;
+        private double[] input;
+        private double[] weights;
+
+        // Constructors
+        public Neuron( ref double[] input )
+        {
+            SetInput(ref input);
+            SortWeights();
+        }
+
+        // Setters
+        public void SetInput(ref double[] input)
+        {
+            this.input = input;
+        }
+
+        public void SetWeights(ref double[] weights)
+        {
+            // instanciando classe para geração de números aleatórios
+            Random random = new Random(DateTime.Now.Millisecond);
+
+        }
+
+        // Getters
+
+
+        // Functional Methods
+        /**
+         * Sorteia o peso para cada entrada
+         */
+        private void SortWeights()
+        { 
+        
+        }
+    }
+}
